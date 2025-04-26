3de1c486ffaa6f18777f96757769eb1078c688c5
Harry Breslin <hbre0004@student.monash.edu>
2025-04-26 16:17:02 +1000

    Add an AuthContext wrapper to app to manage logge din user

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import { AppProps } from "next/app";
 import "../styles/globals.css";
+import { AuthProvider } from "../lib/AuthContext";
+import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
